097a5dbd52d20785d52147b4c2bf463ed7ef548b
Laurent Castañeda <frenando40@hotmai.com>
2019-08-28 04:46:12 -0500

    Reto Cero
    Mostrar el mensaje Hola Mundo

---
+++ Reto Cero/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reto Cero
+//
+//  Created by Laurent castañeda ramirez on 8/28/19.
+//  Copyright © 2019 Laurent castañeda ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
